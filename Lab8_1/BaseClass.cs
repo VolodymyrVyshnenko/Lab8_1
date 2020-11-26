f007f10a6a437397750f592de4af12ce911b9744
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-11-26 12:40:11 +0200

    Добавьте файлы проекта.

---
+++ Lab8_1/BaseClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8_1
+{
+    class BaseClass
+    {
+        private string x;
+        private string y;
+
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return x;
+                }
+                else if (index == 1)
+                {
+                    return y;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (index == 0)
+                {
+                    x = value;
+                }
+                else if (index == 1)
+                {
+                    y = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab8_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseClass = new BaseClass();
+
+            baseClass[0] = "Hello";
+            baseClass[1] = "World";
+
+            Console.WriteLine(baseClass[0] + " " + baseClass[1]);
+            Console.ReadLine();
+        }
+    }
+}
